2fad1b0b05f2fe840d0e1721cffd94ba85d10a52
houssemkharroubi19 <kharroubi.houssem@esprit.tn>
2023-11-03 17:19:24 +0100

    docker compose file and application P correction

--- src/test/java/tn/esprit/rh/achat/services/ProduitServiceImplTest.java
+++ src/test/java/tn/esprit/rh/achat/services/ProduitServiceImplTest.java
@@ -18,7 +18,7 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNull;
 
 @Slf4j
-public class ProduitServiceImplTestmockito {
+public class ProduitServiceImplTest {
 
     @InjectMocks
     private ProduitServiceImpl produitService;
