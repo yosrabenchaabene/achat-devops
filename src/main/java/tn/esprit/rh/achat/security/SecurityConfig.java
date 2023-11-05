4715218abe0e53060483ab8f835b5f4bfe37df9a
houssemkharroubi19 <kharroubi.houssem@esprit.tn>
2023-11-05 18:45:28 +0100

    docker compose file and application P correction

---
+++ src/main/java/tn/esprit/rh/achat/security/SecurityConfig.java
@@ -0,0 +1,21 @@
+package tn.esprit.rh.achat.security; // Adjust the package according to your project's structure
+
+import org.springframework.boot.actuate.autoconfigure.security.servlet.EndpointRequest;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .requestMatchers(EndpointRequest.toAnyEndpoint()).permitAll()
+                .antMatchers("/**").authenticated() // Your other security rules
+                .and()
+                .httpBasic();
+    }
+}
